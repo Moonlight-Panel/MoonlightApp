3c3ebed6958a574f1c2fd2db5a7459d91d15fd7e
Daniel Balk <67603460+Daniel-Balk@users.noreply.github.com>
2023-06-17 17:42:12 +0200

    Update Consts.java

--- app/src/main/java/xyz/moonlightpanel/app/Consts.java
+++ app/src/main/java/xyz/moonlightpanel/app/Consts.java
@@ -7,4 +7,5 @@ public class Consts {
     public static final String APP_URL = "https://my.endelon-hosting.de/";
     //public static final String APP_URL = "http://vps01.so.host.endelon.link:5118/";
     public static final String USER_AGENT = String.format("Moonlight.App/{0} using Gecko/{1}", VERSION, BuildConfig.MOZILLA_VERSION);
+    public static final String USER_AGENT = "Moonlight.App/{0} using Gecko/{1}".replace("{0}", VERSION).replace("{1}", BuildConfig.MOZILLA_VERSION);
 }
