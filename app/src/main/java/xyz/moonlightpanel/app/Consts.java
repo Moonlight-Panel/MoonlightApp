d48089bb068315035d941626f3e05ea413e84287
Daniel Balk <67603460+Daniel-Balk@users.noreply.github.com>
2023-06-18 13:47:58 +0200

    changed version name | added .aab to gitignore

--- app/src/main/java/xyz/moonlightpanel/app/Consts.java
+++ app/src/main/java/xyz/moonlightpanel/app/Consts.java
@@ -3,7 +3,7 @@ package xyz.moonlightpanel.app;
 import org.mozilla.geckoview.BuildConfig;
 
 public class Consts {
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0b";
     public static final String APP_URL = "https://my.endelon-hosting.de/";
     //public static final String APP_URL = "http://vps01.so.host.endelon.link:5118/";
     public static final String USER_AGENT = "Moonlight.App/{0} using Gecko/{1}".replace("{0}", VERSION).replace("{1}", BuildConfig.MOZILLA_VERSION);
